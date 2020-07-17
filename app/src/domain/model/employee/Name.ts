a176006429927dc6320ab3c9e37fc72ff9cf8f43
tkt <tktcorporation.go@gmail.com>
2020-07-18 03:25:45 +0900

    add employee name domain

---
+++ app/src/domain/model/employee/Name.ts
@@ -0,0 +1,22 @@
+import { IsNotEmpty, MaxLength } from 'class-validator';
+
+/**
+ * 氏名
+ */
+export class Name {
+    static readonly 字数制限 = 40;
+
+    @IsNotEmpty()
+    @MaxLength(Name.字数制限)
+    _value: string;
+
+    constructor();
+    constructor(name: string);
+    constructor(name?: string) {
+        this._value = name ?? '';
+    }
+
+    toString(): string {
+        return this._value;
+    }
+}
