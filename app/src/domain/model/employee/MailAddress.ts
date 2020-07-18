7175a67c874c8029c4ee87968abd53f64f1d5ccb
tkt <tktcorporation.go@gmail.com>
2020-07-18 19:16:13 +0900

    add employee mail addredd domain

---
+++ app/src/domain/model/employee/MailAddress.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
+/**
+ * メールアドレス
+ */
+export class MailAddress {
+    @IsNotEmpty()
+    @IsEmail()
+    private _value: string = '';
+
+    constructor();
+    constructor(mailAddress: string);
+    constructor(mailAddress?: string) {
+        this._value = mailAddress ?? '';
+    }
+
+    toString(): string {
+        return this._value;
+    }
+}
