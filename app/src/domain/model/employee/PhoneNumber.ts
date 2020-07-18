8d1f0a2b1061e00cd702b15df0e17631fd601b30
tkt <tktcorporation.go@gmail.com>
2020-07-18 19:32:06 +0900

    add employee phone number domain

---
+++ app/src/domain/model/employee/PhoneNumber.ts
@@ -0,0 +1,27 @@
+import {
+    IsNotEmpty,
+    IsPhoneNumber,
+    MaxLength,
+    MinLength,
+} from 'class-validator';
+
+/**
+ * 電話番号
+ */
+export class PhoneNumber {
+    @IsNotEmpty()
+    @IsPhoneNumber('JP')
+    @MaxLength(13)
+    @MinLength(8)
+    private _value: string = '';
+
+    constructor();
+    constructor(phoneNumber: string);
+    constructor(phoneNumber?: string) {
+        this._value = phoneNumber ?? '';
+    }
+
+    toString(): string {
+        return this._value;
+    }
+}
