cc31f67c41c71af1dcda665d568b106c5ecff3f3
tkt <tktcorporation.go@gmail.com>
2020-07-21 00:57:51 +0900

    update a test

--- app/src/application/repository/EmployeeRepository.spec.ts
+++ app/src/application/repository/EmployeeRepository.spec.ts
@@ -1,33 +1,38 @@
 import 'reflect-metadata';
-import { EmployeeRepository } from 'src/application/repository/EmployeeRepository';
-import { EmployeeDataSource } from 'src/infrastructure/datasource/emploee/EmployeeDataSource';
 import { DBConnection } from 'src/component/database/dbconnection';
 import { EmployeeDao } from 'src/infrastructure/datasource/emploee/EmployeeDao';
 import { QueryRunner } from 'typeorm';
 import { container } from 'tsyringe';
+import { EmployeeRecordCoordinator } from '../coordinator/EmployeeRecordCoordinator';
+import { EmployeeToRegister } from 'src/domain/model/employee/EmployeeToRegister';
+import { MailAddress } from 'src/domain/model/employee/MailAddress';
+import { PhoneNumber } from 'src/domain/model/employee/PhoneNumber';
+import { Name } from 'src/domain/model/employee/Name';
+import { EmployeeDataSource } from 'src/infrastructure/datasource/emploee/EmployeeDataSource';
 
 container.register('ConnectionManager', { useClass: DBConnection });
 container.register('EmployeeMapper', { useClass: EmployeeDao });
+container.register('EmployeeRepository', { useClass: EmployeeDataSource });
 
 describe('EmployeeRepository', () => {
     let queryRunner: QueryRunner;
-    let repository: EmployeeRepository;
+    let coordinator: EmployeeRecordCoordinator;
 
     beforeAll(async () => {
         queryRunner = await DBConnection.getQueryRunner();
-        repository = container.resolve(EmployeeDataSource);
-        //     new EmployeeDataSource(
-        //     new EmployeeDao(queryRunner.manager),
-        // );
+        coordinator = container.resolve(EmployeeRecordCoordinator);
     });
 
     it('should be defined', () => {
-        expect(repository).toBeDefined();
+        expect(coordinator).toBeDefined();
     });
     it('register new', async () => {
-        const number = await repository.registerNew();
-        console.log(number);
-        const employee = await repository.choose(number);
+        const toRegister = new EmployeeToRegister(
+            new Name('name'),
+            new MailAddress('xxxxxxxxx@gmail.com'),
+            new PhoneNumber('000-0000-0000'),
+        );
+        const employee = await coordinator.register(toRegister);
         console.log(employee);
     });
     afterAll(async () => {
