98c48adbfd44e042585462578d1cfe54c2051fbb
tkt <tktcorporation.go@gmail.com>
2020-07-22 06:02:13 +0900

    refactor async decorator

--- app/src/component/decorator/AsyncDecorator.ts
+++ app/src/component/decorator/AsyncDecorator.ts
@@ -7,30 +7,29 @@ export type ErrorArgVoidFunc = ((error: any) => Promise<void>) | (() => void);
  * @param catchFunc error arg void func
  */
 export function AsyncDecorator(
-    beforeFunc: VoidFunc,
-    afterFunc: VoidFunc,
-    catchFunc: ErrorArgVoidFunc,
+    beforeFunc?: VoidFunc,
+    afterFunc?: VoidFunc,
+    catchFunc?: ErrorArgVoidFunc,
 ): MethodDecorator {
     return function (
         target: Object,
         methodName: string | symbol,
         descriptor: PropertyDescriptor,
     ) {
-        // save original method - we gonna need it
         const originalMethod = descriptor.value;
-        // override method descriptor with proxy method
         descriptor.value = function (...args: any[]) {
             const originalMethodFunc = async () => {
                 return originalMethod.apply(this, [...args]);
             };
             const asyncFunc = async () => {
                 try {
-                    await beforeFunc();
+                    await beforeFunc?.();
                     const result = await originalMethodFunc();
-                    await afterFunc();
+                    await afterFunc?.();
                     return result;
                 } catch (e) {
-                    await catchFunc(e);
+                    if (catchFunc === undefined) throw e;
+                    await catchFunc?.(e);
                 }
             };
             return asyncFunc();
