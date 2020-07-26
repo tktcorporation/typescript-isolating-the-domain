6bfedf3d5938772c133e6653363e5120d8532cb9
tkt <tktcorporation.go@gmail.com>
2020-07-26 12:59:09 +0900

    remove a constructor by undefined

--- app/src/domain/model/employee/EmployeeNumber.ts
+++ app/src/domain/model/employee/EmployeeNumber.ts
@@ -2,21 +2,19 @@
  * 従業員番号
  */
 export class EmployeeNumber {
-    private _value?: number;
+    private _value: number;
 
     static from = (value: string) => new EmployeeNumber(Number.parseInt(value));
 
-    constructor();
-    constructor(value: number);
-    constructor(value?: number) {
+    constructor(value: number) {
         this._value = value;
     }
 
-    value(): number | undefined {
+    value(): number {
         return this._value;
     }
 
-    toString(): string | undefined {
-        return this._value?.toString();
+    toString(): string {
+        return this._value.toString();
     }
 }
